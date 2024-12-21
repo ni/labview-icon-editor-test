89c2cc324037c5a8b6f918f3490630e423c37224
sergiov-ni <sergio.velderrain@ni.com>
2024-12-20 19:12:42 -0700

    i

--- .github/scripts/powershell/Set_Development_Mode.ps1
+++ .github/scripts/powershell/Set_Development_Mode.ps1
@@ -1,11 +1,14 @@
-Set-StrictMode -Version Latest
-$ErrorActionPreference = 'Stop'
-$DebugPreference = 'Continue'
-
 param(
     [Parameter(Mandatory=$true)]
     [string]$RelativePath
 )
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+$DebugPreference = 'Continue'
+
+Write-Debug "Set_Development_Mode.ps1: Starting"
+Write-Debug "RelativePath: $RelativePath"
 
 # Validate that $RelativePath exists
 if (!(Test-Path $RelativePath)) {
@@ -20,20 +23,20 @@ try {
     Write-Debug "Adding token to LabVIEW (32-bit)"
     .\AddTokenToLabVIEW.ps1 -MinimumSupportedLVVersion 2021 -SupportedBitness 32 -RelativePath "$RelativePath"
     
-#    Write-Debug "Preparing LabVIEW source (32-bit)"
-#    .\Prepare_LabVIEW_source.ps1 -MinimumSupportedLVVersion 2021 -SupportedBitness 32 -RelativePath "$RelativePath" -LabVIEW_Project #"lv_icon_editor" -Build_Spec "Editor Packed Library"
+    Write-Debug "Preparing LabVIEW source (32-bit)"
+    .\Prepare_LabVIEW_source.ps1 -MinimumSupportedLVVersion 2021 -SupportedBitness 32 -RelativePath "$RelativePath" -LabVIEW_Project #"lv_icon_editor" -Build_Spec "Editor Packed Library"
     
-#    Write-Debug "Closing LabVIEW (32-bit)"
-#    .\Close_LabVIEW.ps1 -MinimumSupportedLVVersion 2021 -SupportedBitness 32
+    Write-Debug "Closing LabVIEW (32-bit)"
+    .\Close_LabVIEW.ps1 -MinimumSupportedLVVersion 2021 -SupportedBitness 32
 
-#    Write-Debug "Adding token to LabVIEW (64-bit)"
-#    .\AddTokenToLabVIEW.ps1 -MinimumSupportedLVVersion 2021 -SupportedBitness 64 -RelativePath "$RelativePath"
+    Write-Debug "Adding token to LabVIEW (64-bit)"
+    .\AddTokenToLabVIEW.ps1 -MinimumSupportedLVVersion 2021 -SupportedBitness 64 -RelativePath "$RelativePath"
     
-#    Write-Debug "Preparing LabVIEW source (64-bit)"
-#    .\Prepare_LabVIEW_source.ps1 -MinimumSupportedLVVersion 2021 -SupportedBitness 64 -RelativePath "$RelativePath" -LabVIEW_Project #"lv_icon_editor" -Build_Spec "Editor Packed Library"
+    Write-Debug "Preparing LabVIEW source (64-bit)"
+    .\Prepare_LabVIEW_source.ps1 -MinimumSupportedLVVersion 2021 -SupportedBitness 64 -RelativePath "$RelativePath" -LabVIEW_Project #"lv_icon_editor" -Build_Spec "Editor Packed Library"
     
-#    Write-Debug "Closing LabVIEW (64-bit)"
-#    .\Close_LabVIEW.ps1 -MinimumSupportedLVVersion 2021 -SupportedBitness 64
+    Write-Debug "Closing LabVIEW (64-bit)"
+    .\Close_LabVIEW.ps1 -MinimumSupportedLVVersion 2021 -SupportedBitness 64
 
     Write-Host "All scripts executed successfully."
 } catch {
